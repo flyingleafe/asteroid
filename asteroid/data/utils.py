9de0762bd9957d2e058b04884345aa6d7cea5e96
Dmitrii Mukhutdinov <flyingleafe@gmail.com>
2020-12-19 17:03:29 +0000

    Add dataset instance for TIMIT + noises with given SNR

--- asteroid/data/utils.py
+++ asteroid/data/utils.py
@@ -1,6 +1,12 @@
 import torch
+import os
+import os.path
+import numpy as np
+import soundfile as sf
+from torch.utils import data
 from torch.utils.data._utils.collate import default_collate
 
+from pathlib import Path
 
 def online_mixing_collate(batch):
     """Mix target sources to create new mixtures.
@@ -21,3 +27,62 @@ def online_mixing_collate(batch):
     targets = torch.stack(new_src, dim=1)
     inputs = targets.sum(1)
     return inputs, targets
+
+
+def cal_adjusted_rms(clean_rms, snr):
+    a = float(snr) / 20
+    noise_rms = clean_rms / (10 ** a)
+    return noise_rms
+
+def cal_rms(amp):
+    return np.sqrt(np.mean(np.square(amp), axis=-1))
+
+def add_noise_with_snr(clean, noise, snr):
+    clean_rms = cal_rms(clean)
+    noise_rms = cal_rms(noise)
+    adjusted_noise_rms = cal_adjusted_rms(clean_rms, snr)
+    
+    noise = noise * (adjusted_noise_rms / noise_rms)
+    
+    mixed = clean + noise
+    alpha = 1.0
+    
+    if mixed.max(axis=0) > 1 or mixed.min(axis=0) < -1:
+        if mixed.max(axis=0) >= abs(mixed.min(axis=0)):
+            alpha = 1. / mixed.max(axis=0)
+        else:
+            alpha = -1. / mixed.min(axis=0)
+        mixed = mixed * alpha
+
+    return mixed
+
+
+def find_audio_files(path, exts=[".wav"]):
+    audio_files = []
+    for root, folders, files in os.walk(path, followlinks=True):
+        for file in files:
+            file = Path(root) / file
+            if file.suffix.lower() in exts:
+                audio_files.append(str(file.resolve()))
+                
+    return audio_files
+
+
+class WavSet(data.Dataset):
+    """
+    Generic dataset class (fetches wav files from a designated folder)
+    """
+    def __init__(self, data_dir, with_path=False):
+        self.with_path = with_path
+        all_files = os.listdir(data_dir)
+        self.wav_paths = [os.path.join(data_dir, p) for p in all_files if p.endswith(".wav")]
+        
+    def __len__(self):
+        return len(self.wav_paths)
+    
+    def __getitem__(self, idx):
+        path = self.wav_paths[idx]
+        audio =  sf.read(path, dtype='float32')[0]
+        if self.with_path:
+            return audio, path
+        return audio
