ce4bfb51707db57944029a713c0ca8fbf37ade72
aaa18-txk02 <ashish.alex10@gmail.com>
2021-06-11 20:31:11 +0100

    process all easy denoise - cleanup

--- notebooks/easy_denoising.py
+++ notebooks/easy_denoising.py
@@ -13,14 +13,6 @@ def denoise_audio(audio_path, model, denoised_file_path):
     denoised = model(noisy).detach().flatten().cpu().numpy()
     sf.write(denoised_file_path, denoised, samplerate=8000)
 
-models_dict = {
-        'RegressionFCNN',
-        'SMoLnet',
-        'DCUNet',
-        'DPTNet',
-        'WaveUNet'
-        }
-
 baseline_model = RegressionFCNN.from_pretrained('Drone_Models_selected/baseline_model_v1.pt')
 smolnet_model = SMoLnet.from_pretrained('Drone_Models_selected/SMoLnet.pt')
 dcunet_model = DCUNet.from_pretrained('Drone_Models_selected/dcunet_20_random_v2.pt')
