668b99cf188040bd78625c5cc7cc3077fb38ede8
aaa18-txk02 <ashish.alex10@gmail.com>
2021-06-11 20:27:47 +0100

    process all easy denoise

--- notebooks/easy_denoising.py
+++ notebooks/easy_denoising.py
@@ -13,6 +13,14 @@ def denoise_audio(audio_path, model, denoised_file_path):
     denoised = model(noisy).detach().flatten().cpu().numpy()
     sf.write(denoised_file_path, denoised, samplerate=8000)
 
+models_dict = {
+        'RegressionFCNN',
+        'SMoLnet',
+        'DCUNet',
+        'DPTNet',
+        'WaveUNet'
+        }
+
 baseline_model = RegressionFCNN.from_pretrained('Drone_Models_selected/baseline_model_v1.pt')
 smolnet_model = SMoLnet.from_pretrained('Drone_Models_selected/SMoLnet.pt')
 dcunet_model = DCUNet.from_pretrained('Drone_Models_selected/dcunet_20_random_v2.pt')
@@ -25,9 +33,19 @@ waveunet_model = WaveUNet.from_pretrained('Drone_Models_selected/waveunet_model_
 # dptnet_model = DPTNet.from_pretrained('/jmain01/home/JAD007/txk02/aaa18-txk02/Datasets/Drone_Models_selected/dptnet_model.pt')
 # waveunet_model = WaveUNet.from_pretrained('/jmain01/home/JAD007/txk02/aaa18-txk02/Datasets/Drone_Models_selected/waveunet_model_adapt.pt')
 
+
+models_dict = {
+        'RegressionFCNN': baseline_model,
+        'SMoLnet': smolnet_model,
+        'DCUNet': dcunet_model,
+        'DPTNet': dptnet_model,
+        'WaveUNet': waveunet_model
+        }
+
 noisy_path = 'test1.wav'
-model = smolnet_model
-# denoised_file_path = audio_path + 'denoised.wav'
 denoised_file_path =  'denoised.wav'
-denoise_audio(noisy_path, baseline_model, denoised_file_path)
+for name, model in models_dict.items():
+    print('processing: ', name)
+    denoised_file_path = str(name) + '.wav'
+    denoise_audio(noisy_path, model, denoised_file_path)
 
